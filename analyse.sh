b5328bd7e4ec8653d190e371672df38a341f0f63
Alain Vagner <alain.vagner@sip.etat.lu>
2022-08-30 09:47:48 +0200

    fixed an issue in the count of the total number of files

--- analyse.sh
+++ analyse.sh
@@ -1,3 +1,4 @@
 for i in ./crawled_files/*; do j=$(basename $i); find ./crawled_files/$j/*.pdf | while read k; do if [ ! -d $k ]; then echo $k; python3 ./pdfCheck.py tocsv  $j $k; fi done done
-for i in ./crawled_files/*; do echo `basename $i`,`find  $i  -type f  | wc -l`,`find  $i -not -name '*.pdf'  -type f  | wc -l`; done | sort -n > ./out/distribution.csv
+echo "site,files,not-pdf" > ./out/distribution.csv
+for i in ./crawled_files/*; do echo `basename $i`,`find  $i  -type f  | wc -l`,`find  $i -not -name '*.pdf'  -type f  | wc -l`; done | sort -n >> ./out/distribution.csv
 node ./docAnalysis.js
